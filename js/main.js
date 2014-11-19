a4c48c214046199a1de6c7a06120ec4b5f97bc8a
Brandon Sprague <brandon00sprague@gmail.com>
2014-11-19 00:06:17 -0500

    Color change and improving the look
    Bigger text overall, better placement on page

--- js/main.js
+++ js/main.js
@@ -13,11 +13,12 @@ function renderText(index) {
   var textDiv = $('<div class="text"></div>');
   textDiv.text(texts[index]);
   textDiv.css({opacity: 0.1 + Math.random() * 0.9});
-  textDiv.css({top: Math.floor(Math.random() * height - height/2)});
-  textDiv.css({width: (20 + Math.random() * 80) + "%" });
-  textDiv.css({'font-size': (0.25 + Math.random() * 3) + "em"});
+  textDiv.css({width: (20 + Math.random() * 55) + "%" });
+  textDiv.css({'font-size': (1 + Math.random() * 3) + "em"});
   body.prepend(textDiv);
-  textDiv.animate({right: "-100%"}, 20000 * Math.random() + 15000, "linear", function() {
+  var textHeight = textDiv.height();
+  textDiv.css({top: Math.floor(Math.random() * height - textHeight/2)});
+  textDiv.animate({right: "-75%"}, 20000 * Math.random() + 15000, "linear", function() {
     $(this).remove();
     renderText(index);
   });
